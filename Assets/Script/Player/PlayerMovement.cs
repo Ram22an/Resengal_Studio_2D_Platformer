87db969479784597df5ba4c9332b1ce50747ce7c
Ram22an <118821139+Ram22an@users.noreply.github.com>
2025-06-16 17:05:49 +0530

    final code

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -11,13 +11,18 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckRadius = 0.1f;
     [SerializeField] private LayerMask groundLayer;
 
-    public int maxJumps = 2;        
+    public int maxJumps = 1;        
     private int currentJumpCount = 0;
 
     [SerializeField]private Transform MyTransforms;
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(groundCheck.position, groundCheck.position + Vector3.down * 0.2f);
+    }
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -55,6 +60,11 @@ public class PlayerMovement : MonoBehaviour
         // Horizontal movement
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
 
+        if (IsGrounded())
+        {
+            currentJumpCount = 0;
+        }
+
         // Jump if pressed and grounded
         if (jumpPressed && currentJumpCount < maxJumps)
         {
@@ -62,14 +72,14 @@ public class PlayerMovement : MonoBehaviour
             currentJumpCount++;
             jumpPressed = false; // reset jump input
         }
-        if (IsGrounded())
-{
-    currentJumpCount = 0;
-}
+
     }
 
     bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        RaycastHit2D hit = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.2f, groundLayer);
+        return hit.collider != null;
     }
+
 }
+
